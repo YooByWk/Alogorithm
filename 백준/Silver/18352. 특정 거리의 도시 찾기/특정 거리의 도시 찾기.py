a01f9ae8ff39902f5b8a956b0d3ac382f70a7890
YooByWk <139323278+YooByWk@users.noreply.github.com>
2023-10-23 16:27:20 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1372 ms, Memory: 161452 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,45 +1,45 @@
-# 단방향 도로
-import sys
-input = sys.stdin.readline
 from collections import deque
-def bfs(X):
-    visited = [False] * (N+1)
-    q = deque([X])
-    ans = []
-    q.append(X)  # X는 출발 위치다
-    visited[X] = True
-    dis = [0] * (N+1)
-    dis[X] = 0
-
-    while q:
-        tmp = q.popleft()
-        for t in near[tmp]:
-            if not visited[t] :
-                q.append(t)
-                visited[t] = True
-                dis[t] = dis[tmp] + 1
-                if dis[t] == K:
-                    ans.append(t)
-    return ans
 
 
-N, M, K, X = map(int,input().split())  # 도시 수, 도로 개수 ,거리 정보, 출발 번호
-# 최단거리가 정확히 K인 장소만을 찾아라.
-lst = []
+def bfs(start):
+    visited[start] = True
+    deq = deque()
+    deq.append(start)
+
+    while deq:
+        temp = deq.popleft()
+        for go in city[temp]:
+            if not visited[go]:
+                visited[go] = True
+                deq.append(go)
+                dis[go] = dis[temp] + 1
+
+                if dis[go] == K:
+                    ans.append(go)
+
+
+
+
+N, M, K, X = map(int,input().split())
+
+city = [ [] for i in range(N+1)]
+# N 도시 개수
+# X 출발
+# 최단거리가 K
+visited = [False] * (N+1)
+ans = []
+dis = [0] * (N+1)
 for i in range(M):
-    k = list(map(int,input().split()))
-    lst.append(k)
-# print(lst)
-near = [[] for _ in range(N+1)]
-# print(near)
-for i in lst:
-    a,b = i
-    near[a].append(b)
-# print(near)
-if len(bfs(X)) ==0:
+    temp_a, temp_b = map(int, input().split())
+    city[temp_a].append(temp_b)
+# print(city)
+bfs(X)
+
+if not ans:
     print(-1)
 else:
-    kkk = bfs(X)
-    kkk.sort()
-    for i in kkk:
+    ans.sort()
+    for i in ans:
         print(i)
+
+
