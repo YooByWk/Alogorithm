51c7af3e5c9cb92f93d0dd3c8b3b65aeed483d5b
YooByWk <139323278+YooByWk@users.noreply.github.com>
2024-02-26 17:54:36 +0900

    [Bronze V] Title: A+B, Time: 148 ms, Memory: 9336 KB -BaekjoonHub

---
+++ node.js/백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let num = Number(input);
+// console.log(input)
+// console.log('asdasdasd')
+a = Number(input[0])
+b = Number(input[1])
+console.log(a + b)
