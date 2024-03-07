2570744318695159680aacc202d63cf50925beaf
YooByWk <139323278+YooByWk@users.noreply.github.com>
2024-03-08 00:48:49 +0900

    [level 0] Title: 코드 처리하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,25 @@
+function solution(code) {
+    var answer = '';
+    let mode = true
+    code = [...code]
+    for (let i = 0; i < code.length; i++) {
+        if (code[i] === '1') {
+            mode = !mode
+            continue
+        }
+        
+        if (mode === true) { // 모드가 0일 때
+            if (i % 2 === 0) { // 짝수
+                answer += code[i]
+                
+            } 
+        } else // 모드가 1일 때
+        if (i % 2 === 1 ) { // 홀수
+            answer += code[i]
+        } 
+    }
+    if (answer === '') {
+        answer = 'EMPTY'
+    }
+    return answer;
+}
