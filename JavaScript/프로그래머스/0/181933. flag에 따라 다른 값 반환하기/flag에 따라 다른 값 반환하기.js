3a61ff3dd682c93c475abe80d653450689a5a3a1
YooByWk <139323278+YooByWk@users.noreply.github.com>
2024-03-08 00:37:18 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.js
@@ -0,0 +1,11 @@
+function solution(a, b, flag) {
+    var answer = 0;
+    switch (flag) {
+        case true:
+        answer = a + b
+        break;
+        case false:
+        answer = a - b
+        break;}
+    return answer;
+}
