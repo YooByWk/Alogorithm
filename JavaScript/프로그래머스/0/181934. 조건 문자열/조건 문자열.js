8b52560e1a937eae4a7290c17ada14cdcd02b488
YooByWk <139323278+YooByWk@users.noreply.github.com>
2024-03-08 00:35:22 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,14 @@
+function solution(ineq, eq, n, m) {
+    var answer = null;
+    if (ineq === '>' && eq ==='=') {
+        n >= m ? answer = 1 : answer = 0;
+    }else if (ineq === '<' && eq === '=') {
+        n <= m ? answer = 1 :answer =  0;
+    }else if (ineq === '>' && eq === '!') {
+        n > m ? answer =  1 : answer =  0
+    }else {
+        n < m ? answer = 1 : answer =  0
+    }
+        
+    return answer;
+}
