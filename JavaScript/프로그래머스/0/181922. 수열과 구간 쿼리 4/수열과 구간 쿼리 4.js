105e6b42009d6823587b33f2bb7bd576d6c7200b
YooByWk <139323278+YooByWk@users.noreply.github.com>
2024-07-15 19:40:55 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 61.21 ms, Memory: 37 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
+++ JavaScript/프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.js
@@ -1,13 +1,28 @@
-function solution(arr, queries) {
-    var answer = [...arr];
-    for (let i = 0; i < queries.length; i++) {
-        [s,e,k] = queries[i];
-        for (let j = s; j <=e;j++ ) {
-            if (j % k === 0) {
-                answer[j] += 1
+// function solution(arr, queries) {
+//     var answer = [...arr];
+//     for (let i = 0; i < queries.length; i++) {
+//         [s,e,k] = queries[i];
+//         for (let j = s; j <=e;j++ ) {
+//             if (j % k === 0) {
+//                 answer[j] += 1
+//             }
+//         }
+//     }
+//     return answer;
+//     // return [s,e,k]
+// }
+
+function solution (arr, queries) {
+    
+    for (let query of queries) {
+        let [s,e,k] = query
+        
+        for (let i = s; i <= e; i++ ) {
+            if (i % k === 0) {
+                arr[i]++
             }
         }
     }
-    return answer;
-    // return [s,e,k]
+       return arr
+    
 }
