942936158e214b8766722e3d837a40cfc7a53edf
YooByWk <139323278+YooByWk@users.noreply.github.com>
2024-07-15 20:28:59 +0900

    [level 0] Title: 배열 만들기 2, Time: 334.69 ms, Memory: 37.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
+++ JavaScript/프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,20 +1,38 @@
-function solution(l, r) {
-    var answer = [];
-    for (let i = l ; i <= r; i ++) {
-        if (i % 5 !== 0) {
-            continue
-        }
-        var str = String(i)
-        if(![...str].every(
-            (s) => {
-           return s === '5' || s ==='0';
-        }
-                          )
-          ) {
-            continue
-        }
-        answer.push(i)
-    }
+// function solution(l, r) {
+//     var answer = [];
+//     for (let i = l ; i <= r; i ++) {
+//         if (i % 5 !== 0) {
+//             continue
+//         }
+//         var str = String(i)
+//         if(![...str].every(
+//             (s) => {
+//            return s === '5' || s ==='0';
+//         }
+//                           )
+//           ) {
+//             continue
+//         }
+//         answer.push(i)
+//     }
     
+//     return answer.length > 0 ? answer : [-1];
+// }
+
+
+function solution(l, r) {
+    let answer = [];
+    for (let i = l; i <= r; i++) {
+        if (i % 5 !== 0) {
+            continue;
+        }
+        let str = String(i);
+        if (![...str].every(
+            (s) => s === '5' || s === '0'
+        )) {
+            continue;
+        }
+        answer.push(i);
+    }
     return answer.length > 0 ? answer : [-1];
 }
