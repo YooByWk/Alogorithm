fa8f508beb9af145822c646d8ea10eb1c6050ee8
YooByWk <tls19190@naver.com>
2025-03-20 20:52:01 +0900

    [Gold IV] Title: 부분합, Time: 176 ms, Memory: 16928 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.js
+++ 백준/Gold/1806. 부분합/부분합.js
@@ -1,37 +1,39 @@
 const fs = require("fs")
 
-const [firstLine, arr] = fs
+const [n, T] = fs
   .readFileSync("/dev/stdin")
   .toString()
   .trim()
   .split("\n")
-// N = 수열의 길이
-// S = 합이 S 이상
-const [N, S] = firstLine.split(" ").map((v) => +v)
+  .map((el) => el.split(" ").map(Number))
 
-const numbers = arr.split(" ").map((v) => +v)
+const S = n[1]  // 목표 합
 
-console.log(solution(numbers, S))
+let start = 0
+let end = 0 
+let len = 1
+let maxLen = 1000000  
 
-function solution(numbers, S) {
-  // 투 포인터
-  let start = 0
-  let end = 0
-  let sum = 0
-  let minLen = Infinity
+// 초기 합 설정
+let sum = T[0]
 
-  while (end <= numbers.length) {
-    // 누적된 합이 S 이상이면
-    if (sum >= S) {
-      minLen = Math.min(minLen, end - start)
-      sum -= numbers[start]
-      start++
-    }
-    // 누적된 합이 S 미만이면
-    else if (sum < S) {
-      sum += numbers[end]
+while (start < T.length) {
+  if (sum < S) { 
+    if (end < T.length - 1) {
       end++
+      len++
+      sum += T[end]
+    } else {
+      break
     }
+  } else { 
+    if (maxLen > len) {
+      maxLen = len
+    }
+    sum -= T[start]
+    start++
+    len--
   }
-  return minLen === Infinity ? 0 : minLen
 }
+
+console.log(maxLen === 1000000 ? 0 : maxLen)
