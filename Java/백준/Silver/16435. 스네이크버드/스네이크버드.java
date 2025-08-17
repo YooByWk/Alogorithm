82b87e208c8d43e03c04b325f2c9cfe675a99856
YooByWk <tls19190@naver.com>
2025-08-17 14:27:38 +0900

    [Silver V] Title: 스네이크버드, Time: 112 ms, Memory: 14664 KB -BaekjoonHub

---
+++ Java/백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -0,0 +1,33 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken()); // 과일의 개수
+    int L = Integer.parseInt(st.nextToken()); // 스네이크 버드의 초기 길이.
+
+    int[] fruits = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      fruits[i] = Integer.parseInt(st.nextToken());
+    }
+
+    Arrays.sort(fruits);
+
+    for (int i = 0; i < N; i++) {
+      if (fruits[i] <= L) {
+        L = (Math.max(fruits[i], L)) + 1;
+      } else {
+        break;
+      }
+    } 
+    System.out.println(L);
+  }
+}
