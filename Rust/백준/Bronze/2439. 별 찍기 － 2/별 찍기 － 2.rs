617fa7f230cb321fe19dfa2898fc9906bc616260
YooByWk <tls19190@naver.com>
2025-10-06 16:25:33 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 13200 KB -BaekjoonHub

---
+++ Rust/백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.rs
@@ -0,0 +1,14 @@
+use std::io::{self, BufWriter, Read, Write, stdout};
+
+pub fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
+    let num_input: usize = input.trim().parse().unwrap();
+    let mut ans = BufWriter::new(stdout().lock());
+    for i in 1..=num_input {
+        write!(ans, "{}", " ".repeat(num_input - i)).unwrap();
+        writeln!(ans, "{}", "*".repeat(i)).unwrap();
+    }
+    return;
+}
